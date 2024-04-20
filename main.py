66cb417f1cbb69de92a0cfd645e41d9d96362fd0
Paweł Fiszer <44177078+pfiszer@users.noreply.github.com>
2024-04-20 04:59:05 +0100

    refactored config

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import configparser
 import gzip
 import pathlib
 import os
@@ -5,104 +6,36 @@ import logging
 import time
 from math import inf as infinity
 
-CONFIG = {"compressionLevel": 5, "rootFolderPath": ".", "filePatterns": [
-    "*.csv"], "loggingLevel": logging.INFO, "timeInterval": 60, "noIterations": infinity}
+config = configparser.ConfigParser()
+config["app"] = {"compressionLevel": 5, "loggingLevel": logging.INFO,
+                 "timeInterval": 60, "iterations": infinity}
+config["filePatterns"] = {}
+config["folderPaths"] = {}
+
+if config.read('settings.ini') == []:
+    with open('settings.ini', "w") as f:
+        config.write(f)
 
 
 def compressAndDelete(filename):
-    with open(filename, "rb") as f_in, gzip.open(f"{filename}.gz", "wb", compresslevel=CONFIG["compressionLevel"]) as f_out:
+    with open(filename, "rb") as f_in, gzip.open(f"{filename}.gz", "wb", compresslevel=config["app"]["compressionLevel"]) as f_out:
         f_out.write(f_in.read())
     os.remove(filename)
 
 
 if __name__ == "__main__":
+
     # Open Config file
-    try:
-        with open("./settings.cfg", "r") as f:
-            lines = f.readlines()
-        for line in lines:
-            if line.startswith("#") or line == "\n":
-                continue
-            else:
-                key, val = line.split("=")
-                val = val.strip()
-                match key:
-                    case "compressionLevel":
-                        try:
-                            val = int(val)
-                            val = abs(val) if abs(val) <= 9 else None
-                        except:
-                            val = None
-                            print("Invalid compression level")
-                    case "loggingLevel":
-                        match val:
-                            case "DEBUG":
-                                val = logging.DEBUG
-                            case "INFO":
-                                val = logging.INFO
-                            case "WARNING":
-                                val = logging.WARNING
-                            case "ERROR":
-                                val = logging.ERROR
-                            case "CRITICAL":
-                                val = logging.CRITICAL
-                            case _:
-                                val = logging.INFO
-                    case "timeInterval":
-                        try:
-                            val = float(val)
-                            val = abs(val)
-                        except:
-                            val = None
-                            print("Invalid time interval")
-                    case "filePatterns":
-                        val = val.split("|")
-                        val = list(map(lambda x: x.strip(), val))
-                    case "rootFolderPath":
-                        val = val.split("|")
-                        val = list(map(lambda x: x.strip(), val))
-                    case "noIterations":
-                        try:
-                            val = int(val)
-                            val = infinity if val < 0 else val
-                        except:
-                            val = None
-                            print("Invalid compression level")
-
-                CONFIG[key] = val if val != None else CONFIG[key]
-        print("Config loaded.")
-    except:
-        with open("settings.cfg", "w") as f:
-            f.write("""## To change the settings from default, enter config keys with values or uncomment following lines
-
-## Compression level is an integer between 0 and 9, higher the compression level, smaller the file, but it takes longer to compress.
-#compressionLevel=5
-
-## Root folder is the folder the program will start searching for the files in, it will search all folders and subfolders in order to find matching files.
-#rootFolderPath=.
-
-## File pattern is the pattern in the name the program will look for when searching for files, use the pipe "|" character to separate patterns
-#filePatterns=*.csv
-
-## Number of iterations, integer that sets the number of times the program scans for files. When set to 0 or lower, it will run indefinetly.
-#noIterations=-1
-
-## Time interval is the time between the start of each iteration of the search measured in seconds (you can include milliseconds by using the dot "." character).
-## It is recommended to set it to 0 when the number of iterations is 1
-#timeInterval=60
-
-## Logging level is the level of onformation saved to the .log file. Acceptable options are: DEBUG | INFO | WARNING | ERROR | CRITICAL
-#loggingLevel=INFO""")
-            quit()
-
-    logging.basicConfig(filename=f'app_{time.strftime("%Y%m%d%H%M%S")}.log', filemode='w',
-                        level=CONFIG["loggingLevel"], format='[%(asctime)s]: {%(levelname)s} %(message)s')
+    if not os.path.exists("./logs"):
+        os.mkdir("./logs")
+    logging.basicConfig(filename=f'./logs/app_{time.strftime("%Y%m%d%H%M%S")}.log', filemode='w',
+                        level=config.getint("app", "loggingLevel", fallback=logging.INFO), format='[%(asctime)s]: {%(levelname)s} %(message)s')
     iteration = 0
-    while iteration < CONFIG["noIterations"]:
+    while iteration < config.getfloat("app", "iterations", fallback=infinity):
         startTime = time.time()
         noFiles = 0
-        for pattern in CONFIG["filePatterns"]:
-            for path in CONFIG["rootFolderPath"]:
+        for pattern in config["filePatterns"].keys():
+            for path in config["folderPaths"].keys():
                 try:
                     folder = pathlib.Path(path).glob(f"**/{pattern}")
                 except:
@@ -122,7 +55,7 @@ if __name__ == "__main__":
                         f"Compression for pattern {pattern} complete.")
         logging.info(f"Compressed {noFiles} files.")
         print(f"Compressed {noFiles} files.")
-        while time.time()-startTime < CONFIG["timeInterval"]:
+        while time.time()-startTime < config.getint("app", "timeInterval"):
             pass
-        if CONFIG["noIterations"] != infinity:
+        if config.getint("app", "iterations", fallback=False):
             iteration += 1
